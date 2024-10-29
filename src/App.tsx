051d882aeb6eb93a3707aefbd449ce4b710dbede
jhansarmiento <jhancsn1@gmail.com>
2024-10-29 16:38:53 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <h1 className="app-title">Cotizador De <span>Cripto Monedas</span></h1>
+    </div>
+
+    <div className="content">
+      
+    </div>
+    </>
+  )
+}
+
+export default App
