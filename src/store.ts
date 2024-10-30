f4e46f22c83af662b6a4e9a048cfa97946e3b1a0
jhansarmiento <jhancsn1@gmail.com>
2024-10-30 17:38:26 -0500

    Termina de tipar la respuesta de la API

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,32 @@
 import { create } from "zustand";
 import axios from "axios";
+import { CryptoCurrenciesResponseSchema } from "./schema/cripto-schema";
+import { CryptoCurrency } from "./types";
+
+type CryptoStore = {
+    cryptoCurrencies: CryptoCurrency[]
+    fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
     
     const {data : { Data }} = await axios(url)
-
-    console.log(Data)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    if(result.success) {
+        return result.data
+    }
 
 }
 
-export const useCryptoStore = create(() => ({
-    fetchCryptos: () => {
-        getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+
+    cryptoCurrencies: [],
+    
+    fetchCryptos: async () => {
+        const cryptoCurrencies = await getCryptos()
+        set(() => ({
+            cryptoCurrencies
+        }))
     }
 }))
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
-import { CurrencySchema } from "../schema/cripto-schema";
+import { CurrencySchema, CryptoCurrencyResponseSchema } from "../schema/cripto-schema";
 
 export type Currency = z.infer<typeof CurrencySchema>
+
+export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
