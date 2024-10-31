dc4e939223a8249091966268c6bf8a5c86cbd9d4
jhansarmiento <jhancsn1@gmail.com>
2024-10-30 20:59:01 -0500

    Valida el formulario

--- src/Components/CriptoSearchForm.tsx
+++ src/Components/CriptoSearchForm.tsx
@@ -1,33 +1,53 @@
 import { useCryptoStore } from "../store";
 import { currencies } from "../data";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Pair } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 export default function CriptoSearchForm() {
 
   const cryptoCurrencies = useCryptoStore((state) => state.cryptoCurrencies)
 
+  // State
   const [pair, setPair] = useState<Pair>({
     currency: '',
     criptocurrency: ''
   })
 
+  // Form Validation
+  const [error, setError] = useState('')
+
+  // Handles changes in the select form
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setPair({
       ...pair,
       [e.target.name]: e.target.value
     })
   }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // Form Validation
+    if(Object.values(pair).includes('')) {
+      setError('Todos Los Campos Son Necesarios')
+      return
+    }
+    setError('')
+  }
   
   return (
-    <form className="form">
-
+    <form 
+      className="form"
+      onSubmit={handleSubmit}  
+    >
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <div className="field">
         <label htmlFor="currency">Moneda: </label>
         <select 
           name="currency" 
           id="currency"
           onChange={handleChange}
+          value={pair.currency}
         >
           <option value="">-- Seleccione --</option>
           {currencies.map((currency) => (
@@ -44,6 +64,7 @@ export default function CriptoSearchForm() {
           name="criptocurrency" 
           id="criptocurrency"
           onChange={handleChange}
+          value={pair.criptocurrency}
         >
           <option value="">-- Seleccione --</option>
           {cryptoCurrencies.map(crypto => (
